d028fd648ed5dd2bc5cd8d1c65d708aa03c94cdb
meghana1312 <80248153+meghana1312@users.noreply.github.com>
2021-07-05 11:50:54 +0530

    Add files via upload

---
+++ mytest2.java
@@ -0,0 +1,16 @@
+import java.sql.*;  
+class mytest2 {  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/example","root","Chandra@12$");  
+//here sonoo is database name, root is username and password  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from cardetails");  
+while(rs.next())  
+System.out.println(rs.getInt("year")+"  "+rs.getString("selling_price")+"  "+rs.getString("name"));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}  
+}  
+}  
